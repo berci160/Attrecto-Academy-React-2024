70b9ae7ad28f0d15594c5f2ac0839dc240c072e8
berci160 <ilyesberci@gmail.com>
2024-09-19 11:01:02 +0200

    1. szorgalmi feladat megoldás

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Navbar from "./components/navbar/Navbar";
+import { Button } from "./components/Button/Button";
 
 function App() {
-
+  const [count, setCount] = useState(0);
+  const [errorMessage,setErrorMessage] = useState(false);
+  const updateCounter = (increase: boolean) => {
+    setCount((currentValue) => {
+      if(increase)
+      {
+        setErrorMessage(false);
+        return currentValue+1;
+      } else if(increase === false && currentValue>0)
+      {
+        return currentValue-1;
+      }
+      else
+      {setErrorMessage(true);
+        return currentValue;
+      }
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
       </header>
+      <div className="container d-flex justify-content-center">
+        <div className="card bg-white shadow text-center p-4 m-4">
+          <div>{errorMessage&&( <h3>Hiba! a számláló nem lehet negatív!</h3>)}</div>
+          <h1>Counter:{count}</h1>
+          <div className="d-flex justify-content-center flex-wrap gap-2">
+            <Button color="primary" onClick={()=>updateCounter(true)}>Increase +</Button>
+            <Button color="secondary" onClick={()=>updateCounter(false)}>Decrease -</Button>
+            <Button color="danger" onClick={()=>setCount(0)}>Reset</Button>
+             
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
