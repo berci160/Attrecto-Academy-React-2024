2753bc417efa36f7fb625229d3e5eb6ad0aada2c
berci160 <ilyesberci@gmail.com>
2024-10-20 10:11:56 +0200

    Session-5-extra-task

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+    
+
+    function Loading() {
+
+      return (
+
+        <div className="loader-container">
+
+          <div className="loader">Please wait, while we load the page:&#41;</div>
+
+
+        </div>
+
+      );
+
+    }
+
+    
+
+    export default Loading;
--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,19 +1,52 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import classNames from 'classnames';
+import Loading from '../loader/Loader';
+import { boolean } from 'yup';
 
 interface PageProps {
     title?: string;
     noCard?: boolean;
     children: ReactNode;
+    isLoading?:boolean;
 }
 
-export const Page = ({title, noCard, children}: PageProps) => {
+export const Page = ({title, noCard, children,isLoading=true}: PageProps) => {
+
+    const[loading, setLoading]=useState(false);
+    const[data,setData]=useState(null);
+
+
+
+    useEffect(()=>{
+
+        const fetchdata=async()=>{
+
+            setLoading(true);
+            try{
+                const resp = await fetch('http://localhost:5173')
+                const ress = await resp.json();
+                setData(ress);
+                setLoading(false)
+            } finally{
+                setLoading(false);
+            }
+           
+        };
+        fetchdata();
+      
+       
+    },[]);
+
     return (
         <div className='container pt-3'>
             {title && <h5>{title}</h5>}
+            {loading?(
+                <Loading/> ):(
             <div className={classNames({"card bg-white shadow p-3": !noCard})}>
                 {children}
             </div>
-        </div>
-    )
+      
+            )}
+              </div>
+    );
 };
